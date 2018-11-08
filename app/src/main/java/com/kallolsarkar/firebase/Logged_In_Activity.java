dd21f6318fbd9477dac0bbaa7e46f29860677469
KallolSarkar <kallol.sarkr@gmail.com>
2018-11-08 21:12:17 +0530

    Add log out button listener

--- app/src/main/java/com/kallolsarkar/firebase/Logged_In_Activity.java
+++ app/src/main/java/com/kallolsarkar/firebase/Logged_In_Activity.java
@@ -1,7 +1,10 @@
 package com.kallolsarkar.firebase;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -20,5 +23,22 @@ public class Logged_In_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged__in_);
+        button = (Button) findViewById(R.id.logout);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(Logged_In_Activity.this, MainActivity.class));
+                }
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+            }
+        });
     }
 }
