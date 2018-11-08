ddbb904d75a0756630e19c10c65278eae8c1d5d4
KallolSarkar <kallol.sarkr@gmail.com>
2018-11-08 21:09:24 +0530

    Initialize logged in activity

--- app/src/main/java/com/kallolsarkar/firebase/Logged_In_Activity.java
+++ app/src/main/java/com/kallolsarkar/firebase/Logged_In_Activity.java
@@ -2,9 +2,20 @@ package com.kallolsarkar.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Logged_In_Activity extends AppCompatActivity {
+    Button button;
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
